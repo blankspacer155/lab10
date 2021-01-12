05a472527b520769066828560c9d87bb04d087ea
blankspacer155 <monnoi.92@gmail.com>
2021-01-12 15:28:30 +0700

    10-1 complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,20 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+
+using namespace std;
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    string grade[9]={"A","B+","B","C+","C","D+","D","F","W"};
+    srand(time(0));
+    int n=rand()%9;
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get "<<grade[n]<<" in this 261102.";
+    return 0;
+}
